8e84ddf4850c7c7b41c7600b986d5afac4a21eb7
Camilo Higuita <chiguitar@unal.edu.co>
2021-04-09 14:36:52 -0500

    initial working version

---
+++ src/code/imagetools_plugin.cpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2020 Carl Schwan <carl@carlschwan.eu>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include <QQmlEngine>
+#include <QResource>
+
+#include "imagetools_plugin.h"
+
+void ImageToolsPlugin::registerTypes(const char *uri)
+{
+#if defined(Q_OS_ANDROID)
+    QResource::registerResource(QStringLiteral("assets:/android_rcc_bundle.rcc"));
+    #endif
+    
+    qmlRegisterType(resolveFileUrl(QStringLiteral("ImageViewer.qml")), uri, 1, 0, "ImageViewer");   
+}
+
